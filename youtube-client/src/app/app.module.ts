382ebedb9c4ab58af4275bc08867b3d6e76802ed
TELEUZI <41520794+TELEUZI@users.noreply.github.com>
2021-07-28 19:50:47 +0300

    refactor: update imports in app module

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { FilterResultsComponent } from './filter/filter-results/filter-results.component';
+import { HeaderComponent } from './header/header/header.component';
 import { SearchItemComponent } from './search/search-item/search-item.component';
-import { FilterResultsComponent } from './search/filter-results/filter-results.component';
-import { LoginInformationComponent } from './header/login-information/login-information.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchResultsComponent, SearchItemComponent, FilterResultsComponent, LoginInformationComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    FilterResultsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
